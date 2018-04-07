ef2ea02c058ab883399b8a3d22939474d77176e2
Chris Dundon <chris.dundon@gmail.com>
2018-04-07 02:23:47 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,13 @@
 class CashRegister
   attr_reader :total
   
-  def initialize()
+  def initialize(emp_discount = 0)
     @total = 0
+  end
+
+  private
+  
+  def disc_fac(disc)
+    @disc_fac = 100 - disc
   end
 end
